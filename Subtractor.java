bceab37badc62813ae4812c9c45addefbfd4cc06
Chandru C <chandru15062004@gmail.com>
2025-12-24 15:25:12 +0530

    Add files via upload

---
+++ Subtractor.java
@@ -0,0 +1,13 @@
+package org.call;
+
+public class Subtractor {
+	public static int number(int a, int b) {
+		return a - b;
+	}
+
+	public static void main(String[] args) {
+		int object = number(454, 5655);
+		System.out.println(object);
+	}
+
+}
