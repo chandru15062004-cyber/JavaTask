8de3c28d34f38d4662161df861bc350e29815288
Chandru C <chandru15062004@gmail.com>
2025-12-24 15:35:56 +0530

    Add files via upload

---
+++ Division.java
@@ -0,0 +1,13 @@
+package org.call;
+
+public class Division {
+	public static int number(int a, int b) {
+		return a / b;
+	}
+
+	public static void main(String[] args) {
+		int object = number(4, 2);
+		System.out.println(object);
+	}
+
+}
