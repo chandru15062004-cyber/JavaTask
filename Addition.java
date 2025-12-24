b846aa3034affead222448f6a29a89ddbf5a2212
Chandru C <chandru15062004@gmail.com>
2025-12-24 15:24:20 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,40 @@
+package org.call;
+
+import java.util.Scanner;
+
+public class Addition {
+	public static int number(int a, int b) {
+		return a + b;
+	}
+	public static void main(String[] args) {
+		int object = number(454, 55);
+		System.out.println(object);
+	}
+}
+	
+//	public static void main(String[] args) {
+//		int sum=(a,b);
+//		Scanner name=new Scanner(System.in);
+//		System.out.println("Enter the naumbers");
+//		int number = name.nextInt();
+//		System.out.println("Enter the operation");
+//		int gg = name.nextInt();
+//		System.out.println("Enter your operatoin");
+//		//int sum =(a+b);
+//		int additon = name.nextInt(a+b);
+//		//int d=(a-b);
+//		int Subtraction = name.nextInt(a-b);
+//		//int f=(a*b);
+//		int Multiplication = name.nextInt(a*b);
+//		//int h=(a/b);
+//		int Division = name.nextInt(a/b);
+//
+//	}
+	
+	
+//				
+//		
+//		
+//	}
+//
+//}
