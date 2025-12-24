5bdd8872990f53b74fc52620b1f8f7456602b733
Chandru C <chandru15062004@gmail.com>
2025-12-24 15:23:13 +0530

    Add files via upload

---
+++ Ktm.java
@@ -0,0 +1,14 @@
+package org.bike;
+
+public class Ktm extends Bike{
+	public void cost(String name) {
+		super.cost(name);		
+}
+	public static void main(String[] args) {
+		Ktm A=new Ktm();
+		A.cost("dwrw");
+		A.speed("skfj");
+	}
+
+
+}
