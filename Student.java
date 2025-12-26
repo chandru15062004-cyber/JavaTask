3d88892417a5c2ed6fefcd5c8fcdf7e7f46c5131
Chandru C <chandru15062004@gmail.com>
2025-12-26 12:09:51 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,11 @@
+package org.collage;
+
+public class Student extends Dept{
+	public void studentName() {
+		System.out.println("Chandru..C");
+	}
+	public void studentDept() {
+		System.out.println("Electronic and Communication");
+	}
+
+}
