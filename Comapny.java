7bf6d280aa9acfd63229b6681352a2c3bc97e687
Chandru C <chandru15062004@gmail.com>
2025-12-27 12:27:34 +0530

    Add files via upload

---
+++ Comapny.java
@@ -0,0 +1,29 @@
+package org.company;
+
+public class Comapny {
+	//method creation
+	private void compname() {
+		System.out.println("wipro");
+	}
+private void compno() {
+	System.out.println("5");
+}
+	public static void main(String[] args)
+	{
+		//create an object 
+		//syntax:className objectName=new className();
+		Comapny d=new Comapny();
+		
+		
+		//method calling
+		//syntax:objectname.methodname();
+		d.compname();
+		d.compno();
+		
+
+				
+				
+			
+	}
+}
+
