81e6ec48c9e0ad845d72f80cd7a8add2ee39e176
Chandru C <chandru15062004@gmail.com>
2025-12-27 12:26:23 +0530

    Add files via upload

---
+++ ComapanyInfo.java
@@ -0,0 +1,24 @@
+package org.company;
+
+public class ComapanyInfo {
+	private void companyName() {
+		System.out.println("CISCO");
+	}
+	private void companyId() {
+		System.out.println("517583");
+	}
+	private void companyAddress() {
+		System.out.println("Chennai");
+	}
+	public static void main(String[] args) {
+		ComapanyInfo WWE=new ComapanyInfo();
+		WWE.companyName();
+		WWE.companyId();
+		WWE.companyAddress();
+		
+		
+		
+			
+	}
+		
+	}
