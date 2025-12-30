83c9465d35a12c4cce213d989f3842a0262bb383
Chandru C <chandru15062004@gmail.com>
2025-12-30 08:35:36 +0530

    Add files via upload

---
+++ SwitchCase.java
@@ -0,0 +1,30 @@
+package org.statement;
+
+public class SwitchCase {
+	public static void main(String[] args) {
+		String Aplhabets = ("");
+		switch (Aplhabets) {
+		case "a":
+			System.out.println("no.1");
+			break;
+		case "b":
+			System.out.println("no.2");
+			break;
+		case "c":
+			System.out.println("no.3");
+			break;
+		case "d":
+			System.out.println("no.4");
+			break;
+		case "e":
+			System.out.println("no.5");
+			break;
+		case "f":
+			System.out.println("no.6");
+			break;
+		default:
+			System.out.println("Not defined");
+		}
+	}
+
+}
