22fb14874f49f97e5503914f2f51a6dee68db2d0
Chandru C <chandru15062004@gmail.com>
2025-12-30 08:33:23 +0530

    Add files via upload

---
+++ ConditionalStatement.java
@@ -0,0 +1,15 @@
+package org.statement;
+
+//"if else" Condition->if 'if' condition is true it executes the if block 
+//(or) if the condition is false then it checks the 'else' block if its true then 'else' block will prints
+public class ConditionalStatement {
+	public static void main(String[] args) {
+		// Initialization
+		int i = 46456;
+		if (i < 15) {
+			System.out.println("yes");
+		} else {
+			System.out.println("no");
+		}
+	}
+}
