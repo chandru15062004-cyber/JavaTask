81bc7fcee96b3f3dd90b44adfc0747a09d066877
Chandru C <chandru15062004@gmail.com>
2025-12-30 08:35:19 +0530

    Add files via upload

---
+++ NestedIf.java
@@ -0,0 +1,18 @@
+package org.statement;
+
+public class NestedIf {
+	// It is used to create 'if' condition inside another 'if' condition
+	public static void main(String[] args) {
+		int j = 50;
+		if (j > 10) {
+			System.out.println("HH");
+		}
+		if (j < 15) {
+			System.out.println("No");
+		} else {
+			System.out.println("DD");
+
+		}
+	}
+
+}
