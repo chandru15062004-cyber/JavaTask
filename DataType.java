14a95aea22725a5aa0fc2acfc4d6542e0c33bd56
Chandru C <chandru15062004@gmail.com>
2026-01-01 11:37:00 +0530

    Add files via upload

---
+++ DataType.java
@@ -0,0 +1,30 @@
+package org.data;
+
+public class DataType {
+	public static void main(String[] argd) {
+	
+		byte overs=20;
+			System.out.println("Remaining overs are " + overs);
+		short matches=5623;
+			System.out.println("Overall T20 matches are " + matches);
+		int runs=22543;
+			System.out.println("Total T20 runs are " + runs);
+		long profit=32545451;
+			System.out.println("Yearly turn over is " + profit);
+		float income=885416.0f;
+		 	System.out.println("My monthly is was " + income);
+		double solution=3.14159;
+			System.out.println("The value of pi is " + solution);
+		String occupation="cooli";
+			System.out.println("The accupation of my father is " + occupation);
+		char result='A';
+			System.out.println("My 10th class grade was " + result);
+		boolean pursuing=true;
+			System.out.println("Are you currently pursuing " + pursuing);	
+	}
+}
+		
+	
+	
+
+
