736fe5bfb1f223d21c2ae73fa1aeff8f640b0581
Chandru C <chandru15062004@gmail.com>
2026-01-02 09:52:45 +0530

    Add files via upload

--- Arts.java
+++ Arts.java
@@ -1,34 +1,17 @@
 package org.edu;
 
-public class Arts {
-	public void bSc(String degree) {
-		System.out.println("Your graduation is : "+degree);
-
-}public void bEd(String degree) {
-	System.out.println("Your graduation is : "+degree);
-}
-	public void bA(String degree) {
-		System.out.println("Your graduation is : "+degree);
+public class Arts extends Engineering {
+	public void bsc() {
+		System.out.println("BSC");
 	}
-		public void bBA(String degree) {
-			System.out.println("Your graduation is : "+degree);
-		}
-		public void ug(String degree) {
-			System.out.println("Your graduation is : "+degree);
-		}
-		public void pg(String degree) {
-			System.out.println("Your graduation is : "+degree);
-		}
-		public static void main(String[] args) {
-			Arts D=new Arts();
-			
-			D.bSc("Arts Group 1");
-			D.bEd("Arts Group 2");
-			D.bA("Arts Gruop 3");
-			D.bBA("Arts Group 4");
-			D.ug("Arts Group 5");
-			D.pg("Arts Group 6");
-			
-		}
-}
+	public void bED(){
+		System.out.println("BED");
+	}
+	public void bA() {
+		System.out.println("BA");
+	}
+	public void bBA() {
+		System.out.println("BBA");
+	}
 
+}
