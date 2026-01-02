d07e572dd4421c4ad877725c51049b7373e5d7fd
Chandru C <chandru15062004@gmail.com>
2026-01-02 09:43:57 +0530

    Add files via upload

---
+++ Arts.java
@@ -0,0 +1,33 @@
+package org.edu;
+
+public class Arts {
+	public void bSc(String degree) {
+		System.out.println("Your graduation is : "+degree);
+
+}public void bEd(String degree) {
+	System.out.println("Your graduation is : "+degree);
+}
+	public void bA(String degree) {
+		System.out.println("Your graduation is : "+degree);
+	}
+		public void bBA(String degree) {
+			System.out.println("Your graduation is : "+degree);
+		}
+		public void ug(String degree) {
+			System.out.println("Your graduation is : "+degree);
+		}
+		public void pg(String degree) {
+			System.out.println("Your graduation is : "+degree);
+		}
+		public static void main(String[] args) {
+			Arts D=new Arts();
+			
+			D.bSc("aa");
+			D.bEd("wer");
+			D.bA("sfnjk");
+			D.bBA("tfutfu");
+			D.ug("add");
+			D.pg("dd");
+			
+		}
+}
