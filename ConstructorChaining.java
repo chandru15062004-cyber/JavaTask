c920f63fe80c00a3544cccf962721d1462ee65a9
Chandru C <chandru15062004@gmail.com>
2026-01-02 09:49:20 +0530

    Rename Asdf class to ConstructorChaining

--- ConstructorChaining.java
+++ ConstructorChaining.java
@@ -1,33 +1,34 @@
 package org.cont;
 
-public class Asdf {
-	public Asdf() {
+public class ConstructorChaining {
+	public ConstructorChaining() {
 		this(1234,12.0f);
 		System.out.println("Finnaly the constructor chaining is succesfully executed");
 	}
-	public Asdf(int i,float f) {
+	public ConstructorChaining(int i,float f) {
 		this("njfsh",132.0f);
 		System.out.println("your account no. is: "+i+"\n"+"Your withdraw is : "+f);
 		
 }
-	public Asdf(String j,float k) {
+	public ConstructorChaining(String j,float k) {
 		this(12.00);
 		System.out.println("your City is: "+j+"\n"+"Your deposition is : "+k);
 		
 	}
-		public Asdf(double g) {
+		public ConstructorChaining(double g) {
 			this(135.0f,"hgf");
 			System.out.println("your accounr has : "+g+"rupess");
 			
 		}	
-			public Asdf(float s,String h) {
+			public ConstructorChaining(float s,String h) {
 				this("hsufh");
 				System.out.println("your salary is: "+s+"\n"+"Your bike name is : "+h);
 			}	
-				public Asdf(String z) {
+				public ConstructorChaining(String z) {
 					System.out.println("your name is: "+z);
 				}
 	public static void main(String[] args) {
 		Asdf D=new Asdf();
 	}
 }
+
