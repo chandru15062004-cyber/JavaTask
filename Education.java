e3f399cbfdcaffdd55798173fd0942c15732c00a
Chandru C <chandru15062004@gmail.com>
2026-01-02 09:44:12 +0530

    Add files via upload

---
+++ Education.java
@@ -0,0 +1,16 @@
+package org.edu;
+
+public class Education extends Arts{
+	public void ug(String degree) {
+		super.ug(degree);
+	}
+	public static void main(String[] args) {
+		Education F=new Education();
+		F.bSc("aa");
+		F.bEd("wer");
+		F.bA("sfnjk");
+		F.bBA("tfutfu");
+		F.ug("edwda");
+		F.pg("shfd");
+	}
+}
