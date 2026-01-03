c7ed744d6bfa9d4ba4e5582ccd148a60e4fa9f3a
Chandru C <chandru15062004@gmail.com>
2026-01-03 07:42:43 +0530

    Add files via upload

---
+++ Medicine.java
@@ -0,0 +1,15 @@
+package org.edu;
+
+public class Medicine extends Arts{
+	public void phySiyo() {
+		System.out.println("123");
+	}
+	public void dental() {
+		System.out.println("Teeth");
+	}
+	public void mbbs() {
+		System.out.println("MBBS");
+	}
+	
+
+}
