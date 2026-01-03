a8384142f4269fbb29383e4dc4d1c22b5c84322c
Chandru C <chandru15062004@gmail.com>
2026-01-03 07:46:49 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,33 @@
+package org.emp;
+
+public class Employee {
+	private void empId1() {
+	System.out.println("111");
+	}
+	private void empName() {
+	System.out.println("chandru");
+	}
+	private void empDob() {
+	System.out.println("09/11/2004");
+	}
+		private void empPhone() {
+			System.out.println("9494822127");
+		}
+		private void empEmail() {
+			System.out.println("chandru15062004@mail.com");
+		}
+		private void empAdress() {
+			System.out.println("Andhra Pradesh");
+		}
+		public static void main(String[] args) {
+	
+			Employee z=new Employee();
+			
+			z.empId1();
+			z.empName();
+			z.empDob();
+			z.empPhone();
+			z.empAdress();
+	}
+
+}
