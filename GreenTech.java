840994639fa68390a4cace392a49d3d8229e2290
Chandru C <chandru15062004@gmail.com>
2026-01-04 20:48:09 +0530

    Add files via upload

---
+++ GreenTech.java
@@ -0,0 +1,28 @@
+package org.add;
+
+public class GreenTech {
+	private void greensOmr() {
+		System.out.println("main");
+	}
+	private void greensAdayar() {
+		System.out.println("1st street");
+	}
+	private void greensThambaram() {
+		System.out.println("2nd street");
+	}
+	private void greensVelachery() {
+		System.out.println("3rd street");
+	}
+	private void greensAnnanagar() {
+		System.out.println("4th street");
+	}
+	public static void main(String[] args){
+		GreenTech k=new GreenTech();
+		k.greensOmr();
+		k.greensAdayar();
+		k.greensThambaram();
+		k.greensVelachery();
+		k.greensAnnanagar();
+				
+	}
+}
