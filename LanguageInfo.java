7b0d7b725274666c9863d416021fc429eb2660a8
Chandru C <chandru15062004@gmail.com>
2026-01-06 22:05:57 +0530

    Add files via upload

---
+++ LanguageInfo.java
@@ -0,0 +1,14 @@
+package org.lang;
+
+public class LanguageInfo {
+
+	public void tamilLanguage() {
+		System.out.println("tamil");
+		}
+	public void englishLanguage() {
+		System.out.println("English");
+	}
+	public void hindiLanguage() {
+		System.out.println("Hindi");
+	}
+}
