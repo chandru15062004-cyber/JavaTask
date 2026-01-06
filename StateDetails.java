588a27807911a2e6cf04c095eadc7479e994790d
Chandru C <chandru15062004@gmail.com>
2026-01-06 22:07:24 +0530

    Add files via upload

--- StateDetails.java
+++ StateDetails.java
@@ -1,21 +1,5 @@
 package org.lang;
 
 public class StateDetails {
-	public void northState() {
-		System.out.println("sf");
-		}
-	public void southstate() {
-		System.out.println("sd");
-	}
-public static void main(String [] args) {
-	LanguageInfo z=new LanguageInfo();
-	z.tamilLanguage();
-	z.englishLanguage();
-	z.hindiLanguage();
-	StateDetails x=new StateDetails();
-	x.northState();
-	x.southstate();
-	
-	
-}
+
 }
