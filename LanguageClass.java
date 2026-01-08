bec460f456e363c379a0d15dcd5d8bfe66c6328e
Chandru C <chandru15062004@gmail.com>
2026-01-08 19:14:48 +0530

    Add files via upload
    Signed-off-by: Chandru C <chandru15062004@gmail.com>

---
+++ LanguageClass.java
@@ -0,0 +1,15 @@
+package org.all;
+import org.telugu.Telugu;
+public class LanguageClass extends Telugu{
+	public void allLanguage() {
+	System.out.println("5444");
+	}
+	public static void main(String[] args) {
+		LanguageClass S=new LanguageClass();
+		S.englishLanguge();
+		S.tamilLanguage();
+		S.teluguLanguage();
+		S.allLanguage();
+	}
+
+}
