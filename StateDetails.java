882e3108c1ebab9516cd9daf870a3fd0df1f991c
Chandru C <chandru15062004@gmail.com>
2026-01-10 17:54:50 +0530

    Add files via upload
    Signed-off-by: Chandru C <chandru15062004@gmail.com>

--- StateDetails.java
+++ StateDetails.java
@@ -1,5 +1,14 @@
 package org.lang;
 
 public class StateDetails {
+	public void tamilLanguage() {
+		System.out.println("Tamil");
+	}
+	public void englishLanguage() {
+		System.out.println("English");
+	}
+	public void hindiLanguage() {
+		System.out.println("Hindi");
+	}
 
 }
