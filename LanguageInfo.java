ef6e998945ab60e421b48b4df4b74078d8fadd21
Chandru C <chandru15062004@gmail.com>
2026-01-10 17:54:32 +0530

    Add files via upload
    Signed-off-by: Chandru C <chandru15062004@gmail.com>

--- LanguageInfo.java
+++ LanguageInfo.java
@@ -1,14 +1,18 @@
 package org.lang;
 
-public class LanguageInfo {
+public class LanguageInfo extends StateDetails{
+	public void southIndia() {
+		System.out.println("South");
+	}
+	public void northIndia() {
+		System.out.println("North");
+	}
+	public static void main(String[] args) {
+		LanguageInfo N=new LanguageInfo();
+		N.tamilLanguage();
+		N.hindiLanguage();
+		N.southIndia();
+		N.northIndia();	
+	}
 
-	public void tamilLanguage() {
-		System.out.println("tamil");
-		}
-	public void englishLanguage() {
-		System.out.println("English");
-	}
-	public void hindiLanguage() {
-		System.out.println("Hindi");
-	}
 }
