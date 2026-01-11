efe9244bd82161b7a77ce92f041ad4ff3d22e344
Chandru C <chandru15062004@gmail.com>
2026-01-11 15:46:56 +0530

    Add files via upload
    Signed-off-by: Chandru C <chandru15062004@gmail.com>

---
+++ ForLoop.java
@@ -0,0 +1,14 @@
+package org.nn;
+
+public class ForLoop {
+	public static void main(String[] args) {
+		for (int i = 0; i <= 10; i++) {
+			if (i % 2 == 0) {
+//	if we continue; statement it skips the condition and prints the remaining values
+				//Continue is used to print "Odd no." 
+			}
+			System.out.println();
+		}
+
+	}
+}
