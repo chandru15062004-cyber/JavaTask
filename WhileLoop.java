cbbb450d21cc83d66078ba622eca7219959efe25
Chandru C <chandru15062004@gmail.com>
2026-01-11 15:48:13 +0530

    Add files via upload
    Signed-off-by: Chandru C <chandru15062004@gmail.com>

---
+++ WhileLoop.java
@@ -0,0 +1,13 @@
+package org.nn;
+
+public class WhileLoop {
+	public static void main(String[] args) {
+		int i=0;
+		while(i<20);{
+			if(i%2==0);
+				System.out.println(i);
+				i++;
+		}
+		
+	}
+	}
