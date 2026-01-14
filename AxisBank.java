44a19354b95c60881e37399c041e12ef4697a198
Chandru C <chandru15062004@gmail.com>
2026-01-14 14:59:24 +0530

    Add files via upload
    Signed-off-by: Chandru C <chandru15062004@gmail.com>

--- AxisBank.java
+++ AxisBank.java
@@ -1,8 +1,14 @@
 package org.bank;
 
-public class AxisBank {
-	public void deposit() {
-		System.out.println("1000");
+public class AxisBank extends BankInfo {
+	public void deposit(float money) {
+		super.deposit(money);
+	}
+	public static void main(String[] args) {
+		AxisBank Z=new AxisBank();
+		Z.fixed(45.2f);
+		Z.saving(1326.1f);
+		Z.deposit(2000.0f);
 	}
 
 }
