dc7bc6c0c4d5395e161cdb18e5895c03a4da6042
Chandru C <chandru15062004@gmail.com>
2026-01-14 14:57:24 +0530

    Add files via upload
    Signed-off-by: Chandru C <chandru15062004@gmail.com>

--- Employee.java
+++ Employee.java
@@ -1,7 +1,21 @@
 package org.emp;
 
 public class Employee {
-	public void empName() {
-		System.out.println("abc");
-	}	
+	public void empId(String Name) {
+		System.out.println("Your Name is: "+Name);
+	}
+	public void empId(int PhoneNo) {
+		System.out.println("Your Phone No. is: "+PhoneNo);
+	}
+	public void empId(float salary) {
+		System.out.println("Credited amount is: "+salary);
+	}
+	public static void main(String[] args) {
+		Employee Z=new Employee();
+		Z.empId("Chandru");
+		Z.empId(01234656);
+		Z.empId(28000.0f);
+	}
+
 }
+
