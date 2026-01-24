e32f34d1eb0436d49bb6faf6746e99d1b5a2bf00
Chandru C <chandru15062004@gmail.com>
2026-01-24 17:38:49 +0530

    Add files via upload
    Signed-off-by: Chandru C <chandru15062004@gmail.com>

---
+++ Wifi.java
@@ -0,0 +1,17 @@
+package org.network;
+
+public class Wifi {
+	public void wifiName() {
+		System.out.println("o");
+	}
+	public static void main(String[] args) {
+		MobileData A=new MobileData();
+		A.dataName();
+		Wireless B=new Wireless();
+		B.modemName();
+		Lan C=new Lan();
+		C.lanName();
+		Wifi F=new Wifi();
+		F.wifiName();
+	}
+}
