52b3385ca680d935709962b041c5d2ec242d9da0
Chandru C <chandru15062004@gmail.com>
2026-01-27 07:49:30 +0530

    Add files via upload
    Signed-off-by: Chandru C <chandru15062004@gmail.com>

---
+++ ChildClass.java
@@ -0,0 +1,16 @@
+package org.inheritance;
+
+public class ChildClass extends ParentClass3{
+	public void subject() {
+		System.out.println("SS");
+	}
+		public static  void main(String[] args) {
+			ChildClass xc=new ChildClass();
+			xc.depart();
+			xc.branch();
+			xc.section();
+			xc.subject();
+			
+		}
+	}
+
