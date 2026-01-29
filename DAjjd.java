7f707134f16623c71748ad0c158b5762e7c0548f
Chandru C <chandru15062004@gmail.com>
2026-01-29 09:07:54 +0530

    Add files via upload
    Signed-off-by: Chandru C <chandru15062004@gmail.com>

---
+++ DAjjd.java
@@ -0,0 +1,15 @@
+package org.hg;
+
+public class DAjjd extends Override{
+	public void name(String M) {
+	System.out.println("jjhh");
+		super.name(M);
+	}
+	public static void main(String[] args) {
+		DAjjd B=new DAjjd();
+		B.name("jjj");
+		B.ggg("hhh");
+		B.jhhuj("dhhswd");
+	}
+
+}
