553f8f614e5a78bed945c3770a50f73dd9720b59
Chandru C <chandru15062004@gmail.com>
2026-01-29 09:09:14 +0530

    Add files via upload
    Signed-off-by: Chandru C <chandru15062004@gmail.com>

---
+++ Override.java
@@ -0,0 +1,22 @@
+package org.hg;
+
+public class Override {
+	public void name(String M) {
+		System.out.println("jkjkjj::"+M);
+	}
+	public void ggg(String M) {
+		System.out.println("jkjkjj::"+M);
+}
+	public void hhu(String M) {
+		System.out.println("jkjkjj::"+M);
+	}
+	public void jhhuj(String M) {
+		System.out.println("jkjkjj::"+M);
+	}
+	public static void main(String[] args) {
+		Override Z=new Override();
+		Z.name("ehue");
+		Z.ggg("hjh");
+		Z.hhu("dhabg");
+	}
+	}
