5c66d7bf58935da4d92a82c65af02c50ec00c40b
Chandru C <chandru15062004@gmail.com>
2026-01-30 10:25:09 +0530

    Add files via upload
    Signed-off-by: Chandru C <chandru15062004@gmail.com>

---
+++ India.java
@@ -0,0 +1,15 @@
+package org.india;
+import org.andhrapradhesh.AndhraPradhesh;
+public class India extends AndhraPradhesh{
+	public void india() {
+		System.out.println("49");
+	}
+	public static void main(String[] args) {
+		India gg=new India();
+		gg.tamilnadu();
+		gg.malayalam();
+		gg.telugu();
+		gg.india();
+	}
+
+}
