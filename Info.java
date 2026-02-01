a711a4e2dc482aaa8f9f33b081cf0ffa987d8494
Chandru C <chandru15062004@gmail.com>
2026-02-01 16:33:45 +0530

    Add files via upload
    Signed-off-by: Chandru C <chandru15062004@gmail.com>

---
+++ Info.java
@@ -0,0 +1,52 @@
+package org.statics;
+
+public class Info {
+	public static float addd() {
+		float addd=121f;
+		return addd;
+	}
+	public static void main(String[] args) {
+		float addd=Info.addd();
+		System.out.println(addd);
+		
+	}
+	}
+
+////	public String name() {
+////		String name="hhtfgiu";
+////		return name;
+////	}
+////	int id;
+////	public void id( int a) {
+////		System.out.println(id);
+////	}
+////	public static float salary() {
+////		float salary=12.0f;
+////		return salary;
+////	}
+//	public static int addition(int c, int b) {
+//		int addition = (c + b);
+//		return addition;
+//
+//	}
+//
+//	public static int multi(int c, int b) {
+//		int multi = (c * b);
+//		return multi;
+//	}
+//
+//	public static void main(String[] args) {
+////		Info nn=new Info();
+////		 nn.name();
+////		System.out.println(nn.name());
+////		Info.salary();
+////		System.out.println(salary());
+//		
+//		int a=Info.addition(4,2);
+//		System.out.println(a);
+//		 int b=Info.multi(2,2);
+//		System.out.println(b);
+//int c=multi(a,b);
+//System.out.println(c);
+//	}
+//}
