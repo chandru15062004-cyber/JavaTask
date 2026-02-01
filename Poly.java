77998ec7994cdd8d20bca670dd7bee9f0a7a0846
Chandru C <chandru15062004@gmail.com>
2026-02-01 16:34:59 +0530

    Add files via upload
    Signed-off-by: Chandru C <chandru15062004@gmail.com>

---
+++ Poly.java
@@ -0,0 +1,26 @@
+package org.statics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Poly {
+	public static void main(String[] args) {
+		List ll= new ArrayList();
+		
+		ll.add(105);
+		ll.add(205);
+		ll.add(305);
+		ll.add(405);
+		ll.add(505);
+		ll.add(605);
+		ll.add(705);
+		ll.add(805);
+		
+		for(int i=0; i<ll.size();i++) {
+			System.out.println(ll.get(i));
+		}
+		
+		
+	}
+
+}
