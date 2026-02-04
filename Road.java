b13796f3ebc70e4e1dacec4b8744dd68cdb8a38e
Chandru C <chandru15062004@gmail.com>
2026-02-04 19:54:29 +0530

    Add files via upload
    Signed-off-by: Chandru C <chandru15062004@gmail.com>

---
+++ Road.java
@@ -0,0 +1,14 @@
+package org.road;
+
+public class Road {
+	public void cycle() {
+		System.out.println("11");
+	}
+	public void bus() {
+		System.out.println("12");
+	}
+	public void car() {
+		System.out.println("13");
+	}
+
+}
